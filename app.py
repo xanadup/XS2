ab8fbb80f6d20e1d1de82e45a5745c60aadec97b
xanadu <xanadup@gmail.com>
2018-03-20 10:42:15 +0900

    add new file

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main():
+    print("Content-type: text/html\n\n")
+    print("<!DOCTYPE html><html><body>Hello world!</body></html>")
+
+
+if __name__ == '__main__':
+    main()
